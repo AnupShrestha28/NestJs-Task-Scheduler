95742045e6a2db6036ea17a3f0b33a0736eee03a
AnupShrestha28 <anupstha0168@gmail.com>
2023-11-03 11:47:05 +0545

    access token generated and routes protected using guards

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,17 @@ import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
   async signup(dto: AuthDto) {
     // generate the password hash
     const hash = await argon.hash(dto.password);
@@ -21,10 +28,7 @@ export class AuthService {
         },
       });
 
-      delete user.hash;
-
-      // return the saved user
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -52,9 +56,26 @@ export class AuthService {
     // if password incorrect throw exception
     if (!pwMatches) throw new ForbiddenException('Credentials Incorrect');
 
-    // send back the user
-    delete user.hash;
-    return user;
-    // return { msg: 'I have signed in' };
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+    return {
+      access_token: token,
+    };
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,8 @@
 /*eslint-disable*/
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
 
-@Module({})
+@Module({
+  controllers: [UserController]
+})
 export class UserModule {}
