46cda9ec56b062c03df9e2547acf487fe1675cb6
AnupShrestha28 <anupstha0168@gmail.com>
2023-11-06 13:46:36 +0545

    user and auth e2e tests done

---
+++ src/user/dto/edit-user.dto.ts
@@ -0,0 +1,16 @@
+/*eslint-disable*/
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class EditUserDto {
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @IsOptional()
+  firstName?: string;
+
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+}
